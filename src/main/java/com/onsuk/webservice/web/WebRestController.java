8259de8ecb7e13be0712e63026990c3d345c4942
onsuk <ondaesuk93@gmail.com>
2018-12-06 20:29:41 +0900

    Spring webservice simple API

---
+++ src/main/java/com/onsuk/webservice/web/WebRestController.java
@@ -0,0 +1,47 @@
+package com.onsuk.webservice.web;
+
+import com.onsuk.webservice.domain.posts.Posts;
+import com.onsuk.webservice.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
